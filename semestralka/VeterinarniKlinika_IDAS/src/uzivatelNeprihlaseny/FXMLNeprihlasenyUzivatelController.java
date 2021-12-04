7bcc60a3f650191f268d9fb2a3b8c774cf33a647
Filda360 <Filda360@seznam.cz>
2021-12-04 20:45:57 +0100

    neprihlasenyUzivatel

--- semestralka/VeterinarniKlinika_IDAS/src/uzivatelNeprihlaseny/FXMLNeprihlasenyUzivatelController.java
+++ semestralka/VeterinarniKlinika_IDAS/src/uzivatelNeprihlaseny/FXMLNeprihlasenyUzivatelController.java
@@ -1,19 +1,26 @@
 
 package uzivatelNeprihlaseny;
 
+import dataTridy.Doktor;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.sql.Blob;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -25,17 +32,17 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
 import static veterinarniklinika.VeterinarniKlinika.con;
 
 public class FXMLNeprihlasenyUzivatelController implements Initializable {
     
+    private ArrayList<Integer> id_doktoru;
 
-    @FXML
-    private TextArea tAreaZprava;
-    @FXML
-    private Button btnOdeslat;
     @FXML
     private ListView<String> listViewDoktori;
     @FXML
@@ -46,7 +53,8 @@ public class FXMLNeprihlasenyUzivatelController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        String query = "SELECT titul, jmeno, prijmeni, telefon, email FROM DOKTORI_UDAJE";
+        id_doktoru = new ArrayList();
+        String query = "SELECT titul, jmeno, prijmeni, telefon, email, id_doktora FROM DOKTORI_UDAJE";
         try (Statement stmt = con.createStatement()) {
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
@@ -55,8 +63,11 @@ public class FXMLNeprihlasenyUzivatelController implements Initializable {
                 String prijmeni = rs.getString("prijmeni");
                 String tel = rs.getString("telefon");
                 String email = rs.getString("email");
+                int id = rs.getInt("id_doktora");
+
                 String doktor = titul + ". " + jmeno + " " + prijmeni + " tel: " + tel + " email: " + email;
                 listViewDoktori.getItems().add(doktor);
+                id_doktoru.add(id);
             }
         } catch (SQLException e) {
             zobrazErrorDialog("Error !", "SQL error !");
@@ -64,23 +75,22 @@ public class FXMLNeprihlasenyUzivatelController implements Initializable {
     }    
 
     //zkouska vlozeni obrazku do databaze
-    @FXML
     private void handleBtnOdeslatOnAction(ActionEvent event) {
         try {
-            PreparedStatement pstmt = con.prepareStatement("INSERT INTO FOTO_DOKTORU(nazev, typ_souboru, pripona, datum_nahrani, obsah, id_doktora) VALUES(?,?,?,?,?,?)");
-            pstmt.setString(1, "default_image");
+            PreparedStatement pstmt = con.prepareStatement("INSERT INTO FOTO_DOKTORU(nazev, typ_souboru, pripona, obsah, id_doktora) VALUES(?,?,?,?,?)");
+            pstmt.setString(1, "jana");
             pstmt.setString(2, "obrazek");
             pstmt.setString(3, ".jpg");
-            pstmt.setDate(4, new Date(50, 5, 5));
+            //pstmt.setDate(4, new Date(50, 5, 5));
             //Inserting Blob type
             InputStream in;
             try {
-                in = new FileInputStream("C:\\users\\42060\\Downloads\\man.jpg");
-                pstmt.setBlob(5, in);
+                in = new FileInputStream("C:\\users\\42060\\Downloads\\jana.jpg");
+                pstmt.setBlob(4, in);
             } catch (FileNotFoundException ex) {
                 zobrazErrorDialog("Chyba", "Obrazek nenalezen");
             }
-            pstmt.setInt(6, 15);
+            pstmt.setInt(5, 24);
             pstmt.execute();
 
         } catch (SQLException ex) {
@@ -107,6 +117,37 @@ public class FXMLNeprihlasenyUzivatelController implements Initializable {
         alert.setHeaderText(headText);
         alert.setContentText(content);
         alert.showAndWait();
+    }
+
+    @FXML
+    private void handleOnMouseClicked(MouseEvent event) {
+        if(listViewDoktori.getSelectionModel().getSelectedItems() != null){ 
+            int index = listViewDoktori.getSelectionModel().getSelectedIndex();
+            try {
+                PreparedStatement pstmt = con.prepareStatement("SELECT obsah FROM foto_doktoru WHERE " + "id_doktora = " + id_doktoru.get(index));
+                ResultSet rs = pstmt.executeQuery();
+                //pstmt.setDate(4, new Date(50, 5, 5));
+                //Inserting Blob type
+                if(rs.next()){ 
+                    byte[] imgData = null;
+                    Blob img = rs.getBlob(1);
+                    imgData = img.getBytes(1,(int)img.length());
+                    BufferedImage image;
+                    try {
+                        image = ImageIO.read(new ByteArrayInputStream(imgData));
+                        Image i = SwingFXUtils.toFXImage(image, null);
+                        iwDoktor.setImage(i);
+                    } catch (IOException ex) {
+                        zobrazErrorDialog("Error !", "Obrázek se nepodarilo načíst");
+                    }
+                }else{ 
+                    iwDoktor.setImage(new Image(getClass().getResourceAsStream("man.jpg")));
+                }
+            } catch (SQLException ex) {
+                zobrazErrorDialog("Chyba", "Chyba vlozeni obrazku");
+            }
+        }
+        
     }
     
 }
