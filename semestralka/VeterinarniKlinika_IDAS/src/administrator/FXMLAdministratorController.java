aca982cf3556520d9a596a7b018f52cbf2e523ac
Filda360 <Filda360@seznam.cz>
2021-12-04 20:47:38 +0100

    neprihlasenyUzivatel

--- semestralka/VeterinarniKlinika_IDAS/src/administrator/FXMLAdministratorController.java
+++ semestralka/VeterinarniKlinika_IDAS/src/administrator/FXMLAdministratorController.java
@@ -3,9 +3,17 @@ package administrator;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
 
 public class FXMLAdministratorController implements Initializable {
+
+    @FXML
+    private ComboBox<?> cbVyberTabulku;
+    @FXML
+    private TableView<?> tableView;
 
 
     @Override
