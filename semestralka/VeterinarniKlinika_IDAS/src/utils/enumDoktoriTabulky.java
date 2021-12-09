a2d305cb9bccb595c9b766399eb1cb177c7b46e9
Filda360 <Filda360@seznam.cz>
2021-12-09 23:28:51 +0100

    doktor controller

--- semestralka/VeterinarniKlinika_IDAS/src/utils/enumDoktoriTabulky.java
+++ semestralka/VeterinarniKlinika_IDAS/src/utils/enumDoktoriTabulky.java
@@ -3,6 +3,6 @@ package utils;
 
 //TABULKY KE KTERÝM MÁ PŘÍSTUP DOKTOR
 public enum enumDoktoriTabulky {
-    Administratori, Biochemie,Diagnozy,Dodavatele,Doktori,Druhy,Faktury, KrevniObrazy,Leciva,Majitele,Objednavky,Odbery,
+    Administratori, Adresy, Biochemie,Diagnozy,Dodavatele,Doktori,Druhy,Faktury, KrevniObrazy,Leciva,Majitele,Objednavky,Odbery,
     Operace,Polozky,Posty, Vysetreni,Zakroky,Zpravy,Zvirata
 }
