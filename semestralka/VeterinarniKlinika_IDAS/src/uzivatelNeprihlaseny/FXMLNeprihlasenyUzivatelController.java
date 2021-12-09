842acfa160c3e1b4e840a941f6bf528b11a962c0
Filda360 <Filda360@seznam.cz>
2021-12-09 18:55:59 +0100

    upravaNeprihlaseneho a prihlaseneho uzivatel

--- semestralka/VeterinarniKlinika_IDAS/src/uzivatelNeprihlaseny/FXMLNeprihlasenyUzivatelController.java
+++ semestralka/VeterinarniKlinika_IDAS/src/uzivatelNeprihlaseny/FXMLNeprihlasenyUzivatelController.java
@@ -30,6 +30,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
@@ -49,6 +50,8 @@ public class FXMLNeprihlasenyUzivatelController implements Initializable {
     private Button btnZpet;
     @FXML
     private ImageView iwDoktor;
+    @FXML
+    private Label labelImg;
 
 
     @Override
@@ -73,11 +76,6 @@ public class FXMLNeprihlasenyUzivatelController implements Initializable {
             zobrazErrorDialog("Error !", "SQL error !");
         }
     }    
-
-    //vlozeni obrazku do databaze
-    private void handleBtnOdeslatOnAction(ActionEvent event) {
-        
-    }
 
     @FXML
     private void handleBtnZpetOnAction(ActionEvent event) {
@@ -105,13 +103,16 @@ public class FXMLNeprihlasenyUzivatelController implements Initializable {
         if(listViewDoktori.getSelectionModel().getSelectedItems() != null){ 
             int index = listViewDoktori.getSelectionModel().getSelectedIndex();
             try {
-                PreparedStatement pstmt = con.prepareStatement("SELECT obsah FROM foto_doktoru WHERE " + "id_doktora = " + id_doktoru.get(index));
+                PreparedStatement pstmt = con.prepareStatement("SELECT obsah, jmeno, mesto FROM foto_doktoru JOIN doktori USING (id_doktora) JOIN adresy USING(id_adresy) JOIN posty USING(id_posty) WHERE " + "id_doktora = " + id_doktoru.get(index));
                 ResultSet rs = pstmt.executeQuery();
                 //pstmt.setDate(4, new Date(50, 5, 5));
                 //Inserting Blob type
                 if(rs.next()){ 
                     byte[] imgData = null;
-                    Blob img = rs.getBlob(1);
+                    Blob img = rs.getBlob("obsah");
+                    String jm = rs.getString("jmeno");
+                    String me = rs.getString("mesto");
+                    labelImg.setText(jm + "\n" + me);
                     imgData = img.getBytes(1,(int)img.length());
                     BufferedImage image;
                     try {
@@ -123,6 +124,7 @@ public class FXMLNeprihlasenyUzivatelController implements Initializable {
                     }
                 }else{ 
                     iwDoktor.setImage(new Image(getClass().getResourceAsStream("man.jpg")));
+                    labelImg.setText("");
                 }
             } catch (SQLException ex) {
                 zobrazErrorDialog("Chyba", "Chyba vlozeni obrazku");
